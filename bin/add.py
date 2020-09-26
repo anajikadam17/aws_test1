b46c57160191b0a458100f2972647b63883029c4
anajikadam17 <anajikadam17@gmail.com>
2020-09-26 10:56:58 +0530

    update requrement

---
+++ bin/add.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:16:35 2020
+
+@author: Anaji
+"""
+
+
+def add_number(a, b):
+    """
+        Addition of two number
+        Input:
+            number: two number a and b
+        Output:
+            number: addition of two number
+    """
+    ans = a + b  # Addition of number
+    return ans
+
+num1 = 12
+num2 = 13
+addition = add_number(num1, num2)
+print(addition)
